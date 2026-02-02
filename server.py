1a71eeb033986b453f6e21b14a59636a3d88116b
adarshm11 <amlearning2023@gmail.com>
2026-02-01 22:55:11 -0800

    playback, skipping, auto play next all works

--- server.py
+++ server.py
@@ -1,6 +1,4 @@
-import os
 import subprocess
-import uuid
 import threading
 from contextlib import asynccontextmanager
 from collections import deque
@@ -9,7 +7,7 @@ import uvicorn
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from pytubefix import YouTube
+from mpv import MPV
 
 from modules.args import get_args
 from modules.logger import logger
@@ -17,21 +15,23 @@ from modules.logger import logger
 
 song_queue = deque()
 queue_lock = threading.Lock()
-jbl_event = threading.Event()
+player = MPV(
+        ytdl=True,
+        input_default_bindings=False,
+        input_vo_keyboard=False,
+        # wid=None,
+        vo='null',
+    )
 
 
 @asynccontextmanager
-async def lifespan():
+async def lifespan(app: FastAPI):
     try:
         connect_to_jbl()
     except Exception as e:
         logger.error(f'Failed to connect to JBL speaker: {e}')
         raise
-    thread = threading.Thread(target=send_songs_to_jbl, daemon=True)
-    thread.start()
     yield
-    jbl_event.set()
-    thread.join()
     try:
         disconnect_jbl()
     except Exception as e:
@@ -49,50 +49,65 @@ app.add_middleware(
 
 args = get_args()
 
-class PlayRequest(BaseModel):
+class QueueRequest(BaseModel):
     url: str
+    play_immediately: bool = False
 
 
 @app.get('/')
 def root():
     return {'message': 'sce jbl is running'}
 
-@app.post('/play')
-def play_song(request: PlayRequest):
+
+@app.post('/queue')
+def enqueue_song(request: QueueRequest):
+    if not request or not request.url:
+        raise HTTPException(status_code=400, detail='Invalid request payload')
     logger.info(f'Received play request for URL: {request.url}')
-    download_video(request.url)
+    if request.play_immediately:
+        with queue_lock:
+            song_queue.appendleft(request.url)
+        play_next_song()
+        return {'message': 'Playing song immediately'}
+    with queue_lock:
+        song_queue.append(request.url)
+    if player.idle_active:
+        play_next_song()
     return {'message': 'Song added to queue'}
+
 
 @app.get('/skip')
 def skip_song():
-    pass
+    play_next_song()
+    return {'message': 'Skipped to next song'}
+
 
 @app.get('/stop')
 def stop():
     if song_queue:
         with queue_lock:
             song_queue.clear()
+        player.stop()
         logger.info('Stopped playback and cleared the queue')
         return {'message': 'Playback stopped and queue cleared'}
+    return {'message': 'No playback to stop'}
 
 
-def download_video(url: str) -> None:
-    '''Downloads a YouTube video as mp3 and adds it to the song queue.''' # at some point try to figure out how to do this without PyTube -> Spotify integration?
-    video = YouTube(url)
-    if video.age_restricted:
-        raise HTTPException(status_code=400, detail='Age restricted video')
+@player.event_callback('end-file')
+def on_end(event):
+    try:
+        event_str = str(event)
     
-    os.makedirs(args.output_dir, exist_ok=True)
-    stream = video.streams.filter(only_audio=True).first()
-    mp4_path = stream.download(output_path=args.output_dir, filename=f'{uuid.uuid4()}.mp4')
+        if "reason" in event_str:
+            start = event_str.find("'reason': b'") + len("'reason': b'")
+            end = event_str.find("'", start)
+            reason = event_str[start:end]
+            if reason == 'eof':
+                play_next_song()
+                
+    except Exception as e:
+        logger.error(f"Failed to handle event: {e}")
 
-    # convert to mp3
-    mp3_path = mp4_path.replace('.mp4', '.mp3')
-    subprocess.run(['ffmpeg', '-i', mp4_path, mp3_path])
-    os.remove(mp4_path)
-    with queue_lock:
-        song_queue.append(mp3_path)
-    logger.info(f'Downloaded and queued song: {mp3_path}')
 
 def connect_to_jbl():
     # first connect to the JBL speaker
@@ -103,6 +118,7 @@ def connect_to_jbl():
 
     # set the audio output to be the JBL speaker
     set_audio_output_to_jbl()
+
 
 def set_audio_output_to_jbl():
     status = subprocess.run(['wpctl', 'status'], capture_output=True, text=True).stdout
@@ -121,18 +137,20 @@ def set_audio_output_to_jbl():
     
     logger.info('JBL speaker not found among audio sinks')
 
+
 def disconnect_jbl():
     subprocess.run(['bluetoothctl', 'disconnect', args.jbl_mac_address], check=True)
     subprocess.run(['wpctl', 'set-default', '@DEFAULT_AUDIO_SINK@'], check=True)
     logger.info('Disconnected from JBL speaker')
 
-def send_songs_to_jbl():
-    while not jbl_event.is_set():
+
+def play_next_song():
+    with queue_lock:
         if song_queue:
-            with queue_lock:
-                song_path = song_queue.popleft()
-            logger.info(f'Sending song to JBL: {song_path}')
-            # send song to JBL speaker using Linux command line
+            next_song = song_queue.popleft()
+            logger.info(f'Playing next song from queue: {next_song}')
+            player.play(next_song)
+
 
 if __name__ == '__main__':
     uvicorn.run('server:app', host=args.host, port=args.port, reload=True)
